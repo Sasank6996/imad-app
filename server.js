20b2d760dc147f630b7d85185d101ce01eab8868
Sasank6996 <32029132+Sasank6996@users.noreply.github.com>
2017-10-29 13:12:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/Go-speedy-Go', function(req,res) {
-    res.send('Go-Speedy-Go is requested and will be served here');
+app.get('/article-one', function(req,res) {
+    res.send('Article one is requested and will be served here');
 });
 
 app.get('/Student registration form', function(req,res) {
